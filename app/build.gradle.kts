0a8b072f19d278693d0ceab402efeff4c97bae90
denis-ismailaj <denis.ismailaj1@gmail.com>
2024-03-21 14:53:33 +0100

    Remove empty tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,6 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
@@ -80,20 +79,12 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation ("androidx.appcompat:appcompat:1.1.0")
     implementation ("androidx.core:core-ktx:1.1.0")
     implementation ("androidx.constraintlayout:constraintlayout:1.1.3")
     implementation ("com.google.android.material:material:1.0.0")
-    testImplementation ("junit:junit:4.12")
-    androidTestImplementation ("androidx.test:runner:1.2.0")
-    androidTestImplementation ("androidx.test.espresso:espresso-core:3.2.0")
     implementation ("com.android.volley:volley:1.2.1")
     implementation ("androidx.legacy:legacy-support-v4:1.0.0")
 
